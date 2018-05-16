40b6e415d3514058f63fc0cf829e496219e1251f
Claudio <claudio.sousa@gmail.com>
2018-05-16 19:32:47 +0200

    rename

--- src/G3_TP2_light_com_rx/scripts/tp2_corr.m
+++ src/G3_TP2_light_com_rx/scripts/tp2_corr.m
@@ -1,15 +1,15 @@
-% Reconstitustion d'un message reçu par signal lumineux à 2 composantes (rouge et bleue)
-% Le fichier contenant le signal d'entrée doit alterner les échantillons clear/RGB
+% Reconstitustion d'un message reï¿½u par signal lumineux ï¿½ 2 composantes (rouge et bleue)
+% Le fichier contenant le signal d'entrï¿½e doit alterner les ï¿½chantillons clear/RGB
 % sous forme "texte" avec un nombre par ligne dans le fichier de votre
-% choix (nom à donner dans 'filename').
+% choix (nom ï¿½ donner dans 'filename').
 clear
 filename='rgb_ref.txt';
-
+graphics_toolkit ("fltk")
 samples_per_bit=5;
-tot_bytes=15;    % nombre d'octets reçus incluant les 2 octets de la séquence de référence
-bits_ref=[1 0 0 1 0 1 0 1 0 0 0 1 1 0 1 1];    % séquence de référence(0x951B)
+tot_bytes=15;    % nombre d'octets reï¿½us incluant les 2 octets de la sï¿½quence de rï¿½fï¿½rence
+bits_ref=[1 0 0 1 0 1 0 1 0 0 0 1 1 0 1 1];    % sï¿½quence de rï¿½fï¿½rence(0x951B)
 
-% construction de la séquence en tenant compte de samples_per_bit
+% construction de la sï¿½quence en tenant compte de samples_per_bit
 seq_ref_red=[];
 seq_ref_blue=[];
 for n=0:2:length(bits_ref)-1
@@ -28,55 +28,55 @@ red=red*max_ratio;            % normalisation amplitude
 
 figure(1)
 plot(1:rlen/4, red, 'r', 1:rlen/4, blue, 'b')
-title('Signaux rouge et bleu reçus normalisés par rapport au signal bleu')
+title('Signaux rouge et bleu reï¿½us normalisï¿½s par rapport au signal bleu')
 
 figure(2)
 clf
 subplot(5,1,1)
 plot(red, 'r')
 axis([0 length(red) -50 1.2*max_sig])
-title('Signal rouge reçu (normalisé)')
+title('Signal rouge reï¿½u (normalisï¿½)')
 
 subplot(5,1,2)
 plot(blue)
 axis([0 length(blue) -50 1.2*max_sig])
-title('Signal bleu reçu')
+title('Signal bleu reï¿½u')
 
 subplot(5,1,3)
 c_red=conv(red,seq_ref_red(end:-1:1));
 corr_res_red=c_red(length(seq_ref_red):end);
 plot(corr_res_red,'r--')
-title('Corrélation du signal rouge avec séquence de référence')
+title('Corrï¿½lation du signal rouge avec sï¿½quence de rï¿½fï¿½rence')
 
 subplot(5,1,4)
 c_blue=conv(blue,seq_ref_blue(end:-1:1));
 corr_res_blue=c_blue(length(seq_ref_blue):end);
 plot(corr_res_blue,'b--')
-title('Corrélation du signal bleu avec séquence de référence')
+title('Corrï¿½lation du signal bleu avec sï¿½quence de rï¿½fï¿½rence')
 
 subplot(5,1,5)
 corr_tot=corr_res_red+corr_res_blue;
 plot(corr_tot,'k')
-% recherche de l'indice du maximum de corrélation sur le premier tiers
+% recherche de l'indice du maximum de corrï¿½lation sur le premier tiers
 % du signal
 [m idx]=max(corr_tot(1:ceil(end/3)));
-sprintf('Position du début de trame: %d', idx)
+sprintf('Position du dï¿½but de trame: %d', idx)
 start=idx(1)+floor(samples_per_bit/2)
-title(sprintf('Corrélation de la somme (rouge+bleu). Première position de S: %d', start))
+title(sprintf('Corrï¿½lation de la somme (rouge+bleu). Premiï¿½re position de S: %d', start))
 
 
-% index des bits de données: 
+% index des bits de donnï¿½es: 
 t=start:samples_per_bit:start+(tot_bytes*8/2-1)*samples_per_bit;  % /2 car 2 composantes
 bits(1:2:tot_bytes*8)=red(t)>max_sig/2;    % le signe de f(t) indique la valeur de chaque bit
 bits(2:2:tot_bytes*8)=blue(t)>max_sig/2;
 
-% Transformation binaire -> hexadécimal -> ASCII
+% Transformation binaire -> hexadï¿½cimal -> ASCII
 z=binaryVectorToHex(bits');
 znum=[];
 for n=5:2:length(z)
     znum=[znum hex2dec(z(n:n+1))];
 end
-% détection de la séquence de référence
+% dï¿½tection de la sï¿½quence de rï¿½fï¿½rence
 if strcmp(z(1:4),'951B')
     sprintf('Reception correcte: %s (%s)\n', [char(znum)], z)
 else
@@ -88,7 +88,7 @@ clf
 subplot(2,1,1)
 range=start:start+(tot_bytes*8-1)*samples_per_bit;  
 plot(range-start+1,red(range),'r')    
-title('Illustration de la détection des bits sur signal rouge alignés sur S')
+title('Illustration de la dï¿½tection des bits sur signal rouge alignï¿½s sur S')
 hold on
 plot(t-start+1, red(t), 'kx', t(1:8)-start+1, bits_ref(1:2:end)*max_sig, 'mo')
 for n=1:samples_per_bit*8:length(range);
@@ -98,7 +98,7 @@ end
 subplot(2,1,2)
 range=start:start+(tot_bytes*8-1)*samples_per_bit; 
 plot(range-start+1,blue(range),'b')    
-title('Illustration de la détection des bits sur signal bleu alignés sur S')
+title('Illustration de la dï¿½tection des bits sur signal bleu alignï¿½s sur S')
 hold on
 plot(t-start+1, blue(t), 'kx', t(1:8)-start+1, bits_ref(2:2:end)*max_sig, 'mo')
 for n=1:samples_per_bit*8:length(range);
