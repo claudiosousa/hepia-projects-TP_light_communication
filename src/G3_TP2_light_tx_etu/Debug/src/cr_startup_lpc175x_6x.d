bd580dfe6a9a613c260b7ce7457386a5e1b1efb7
Claudio <claudio.sousa@gmail.com>
2018-05-17 00:19:17 +0200

    git ignore

--- src/G3_TP2_light_tx_etu/Debug/src/cr_startup_lpc175x_6x.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc175x_6x.d: ../src/cr_startup_lpc175x_6x.c \
- C:\Users\vince\Documents\LPCXpresso_5.2.6_2137\ptr\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h
-
-C:\Users\vince\Documents\LPCXpresso_5.2.6_2137\ptr\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
