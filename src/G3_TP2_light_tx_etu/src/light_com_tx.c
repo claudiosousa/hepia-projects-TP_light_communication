1afa0f028ddec36ed158765d6750bb2c0cc7521c
David Gonzalez <david.dg.gonzalez@etu.hesge.ch>
2018-05-16 21:30:29 +0200

    Sender code (to check if really works)

--- src/G3_TP2_light_tx_etu/src/light_com_tx.c
+++ src/G3_TP2_light_tx_etu/src/light_com_tx.c
@@ -2,25 +2,44 @@
  * emetteur.c
  *
  * Date: 19.05.2015
- * Auteur: V. Pilloux
+ * Auteur: Claudio Sousa, David Gonzalez
  * Description R/B transmitter. Warning: Mylab2 must be powered up too to get enough power !
  */
 #include <cr_section_macros.h>
 #include <NXP/crp.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "LPC17xx.h"
 #include "ledrgb_dma.h"
-
 
 #define RED 0xFF0000
 #define BLUE 0xFF
 #define WHITE 0xFFFFFF
 #define BLACK 0
 
+#define TIMER_CLOCK_S 25000000
+#define TIMER_CLOCK_MS (TIMER_CLOCK_S / 1000)
+#define TIMER_WAIT_MS 8
+#define ASCII_SIZE 8
+#define START_OF_FRAME "\x95\x1B"
+#define MESSAGE "PTR is funny"
+#define SOF_SIZE 2
+#define DATA_SIZE 15
+#define CHECKSUM_SIZE 1
+#define FRAME_SIZE (SOF_SIZE + DATA_SIZE + CHECKSUM_SIZE)
+#define STR_SIZE (FRAME_SIZE + 1)
 
+// Allow the main code to be notified of the timer du date
+bool sleep_done = false;
 
+/**
+ * Create a XOR checksum of the given string
+ * @param s String to checksum
+ * @param len Size of the string
+ * @return Checksum of the string
+ */
 uint8_t calc_checksum(uint8_t *s, int len)
 {
 	int i;
@@ -31,23 +50,63 @@ uint8_t calc_checksum(uint8_t *s, int len)
 	return chksum;
 }
 
-// DO NOT FORGET TO PLUG A SECOND CABLE ON USB CONNECTOR ON MYLAB2 TO GET RIGHT COLORS!
+/**
+ * Timer that handler timing of the sender
+ */
+void TIMER0_IRQHandler(void)
+{
+	LPC_TIM0->IR = 1;
+	sleep_done = true;
+}
 
+/**
+ * Main function
+ */
 int main (void)
 {
-	char *str="PTR is funny", cheksum;
-	int i;
+	// Timer setup
+	LPC_TIM0->MCR = 3;
+	LPC_TIM0->MR0 = TIMER_WAIT_MS - 1;
+	LPC_TIM0->PR = TIMER_CLOCK_MS - 1;
+	LPC_TIM0->TC = 0;
+	NVIC_EnableIRQ(TIMER0_IRQn);
+	LPC_TIM0->TCR = 1;
+	// Message setup
+	// Concatenate the parts and be sure to have a valid string
+	char str[STR_SIZE];
+	uint8_t checksum = calc_checksum((uint8_t *)MESSAGE, STR_SIZE);
+	memset(str, '\0', STR_SIZE);
+	strncat(str, START_OF_FRAME, STR_SIZE);
+	strncat(str, MESSAGE, STR_SIZE);
+	str[FRAME_SIZE - 2] = '\0';
+	str[FRAME_SIZE - 1] = checksum;
+	str[FRAME_SIZE] = '\0';
+	// Counters, one for the string and one for the bits
+	unsigned char c = 0;
+	unsigned int i = 0;
+	// Temporary variables
+	uint32_t color = 0;
 
-	// example of checksum calculation:
-	cheksum=calc_checksum((uint8_t *)str, strlen(str));
-
-	// example of RGB led using:
 	while(1)
 	{
-		set_rgb_leds_color(0, RED);
-		for (i=0; i<10000000; i++);
-		set_rgb_leds_color(0, BLUE);
-		for (i=0; i<10000000; i++);
+		// Wait for the timer to tell us that the date has come
+		while (sleep_done == false) {
+			__WFI();
+		}
+		sleep_done = false;
+
+		// Get current char bits, RED for odd, BLUE for even
+		// From MSB to LSB
+		color = (str[c] & (1 << (ASCII_SIZE - 1 - i)) ? RED : 0) |
+				(str[c] & (1 << (ASCII_SIZE - 2 - i)) ? BLUE : 0);
+		set_rgb_leds_color(0, color);
+
+		// Increment bit counter, two by two
+		i = (i + 2) % ASCII_SIZE;
+		// Increment char counter only if bit counter has reset
+		if (i == 0) {
+			c = (c + 1) % FRAME_SIZE;
+		}
 	}
 }
 
